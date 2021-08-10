42ca369b9c97156c9413796bf7888e96ff747698
eatingdishes <stefanrohner4@gmail.com>
2021-08-10 21:23:41 +0200

    started ui

--- Assets/Scripts/Clintconnects.cs
+++ Assets/Scripts/Clintconnects.cs
@@ -7,19 +7,22 @@ using UnityEngine.UI;
 public class Clintconnects : NetworkManager
 {
     public NetworkConnection connectionDisconnect;
-    public bool disconnect=false;
+    public bool disconnect=false,t=false;
     public mapGenerator mapGenerator;
     public int clintConnectCount = 0,clintDisConnectCount=0;
-    
+    public GameObject canvas;
    
     public override void OnServerConnect(NetworkConnection conn)
     {
         mapGenerator.newClientJoined = true;
         clintConnectCount++;
     }
-    public void hi()
+    public void Update()
     {
-
+       
+        
+            canvas.SetActive(true);
+          
     }
 }
 
---
+++ Assets/hudButtonsTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class hudButtonsTest : NetworkBehaviour
+{
+    public NetworkManager NetworkManager;
+   
+    
+    
+        
+    
+    
+    public void Server()
+    {
+        NetworkManager.StartServer();
+        
+    }
+    public void Client()
+    {
+        NetworkManager.StartClient();
+        
+    }
+   
+}
